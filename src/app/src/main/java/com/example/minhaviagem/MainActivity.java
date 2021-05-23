e40479f24bd7b630e6227358f93774fb77b6f0c2
Jeison O. da Silva <jeison.desenvolvimento@gmail.com>
2021-05-23 18:54:14 -0300

    Configurado as chaves do appcenter e mapa

--- src/app/src/main/java/com/example/minhaviagem/MainActivity.java
+++ src/app/src/main/java/com/example/minhaviagem/MainActivity.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.minhaviagem.R;
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +15,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AppCenter.start(getApplication(), BuildConfig.APPCANTER_API_KEY,
+                Analytics.class, Crashes.class);
+
+
     }
 }
