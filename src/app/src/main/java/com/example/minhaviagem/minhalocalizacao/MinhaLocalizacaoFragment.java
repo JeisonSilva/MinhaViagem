35f4e71114cac148a8e5bed7ab936844b6dbee10
Jeison O. da Silva <jeison.desenvolvimento@gmail.com>
2021-05-23 19:09:13 -0300

    limpesa da class localizacao

--- src/app/src/main/java/com/example/minhaviagem/minhalocalizacao/MinhaLocalizacaoFragment.java
+++ src/app/src/main/java/com/example/minhaviagem/minhalocalizacao/MinhaLocalizacaoFragment.java
@@ -17,33 +17,12 @@ import com.example.minhaviagem.R;
  */
 public class MinhaLocalizacaoFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
     public MinhaLocalizacaoFragment() {
         // Required empty public constructor
     }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MinhaLocalizacaoFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static MinhaLocalizacaoFragment newInstance(String param1, String param2) {
         MinhaLocalizacaoFragment fragment = new MinhaLocalizacaoFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -52,8 +31,6 @@ public class MinhaLocalizacaoFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
 
