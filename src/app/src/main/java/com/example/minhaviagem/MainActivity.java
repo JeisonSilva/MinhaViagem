a058a136a527e8834f2d6ac93ddcb4e4029e9214
Jeison O. da Silva <jeison.desenvolvimento@gmail.com>
2021-05-27 00:11:57 -0300

    Layout da tela principal.
    Melhorado a tela principal separando as funcionalidades em um menu.

--- src/app/src/main/java/com/example/minhaviagem/MainActivity.java
+++ src/app/src/main/java/com/example/minhaviagem/MainActivity.java
@@ -1,28 +1,43 @@
 package com.example.minhaviagem;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-import com.example.minhaviagem.R;
 import com.example.minhaviagem.minhalocalizacao.MinhaLocalizacaoFragment;
-import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.material.navigation.NavigationView;
 import com.microsoft.appcenter.AppCenter;
 import com.microsoft.appcenter.analytics.Analytics;
 import com.microsoft.appcenter.crashes.Crashes;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    DrawerLayout drawerLayout;
+    Toolbar toolbar;
+    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        drawerLayout = findViewById(R.id.drawer);
+        toolbar = findViewById(R.id.toolbar);
+        navigationView = findViewById(R.id.navigarion);
+
+        setSupportActionBar(toolbar);
+        navigationView.setNavigationItemSelectedListener(this);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.openDrawer, R.string.closeDrawer);
+        toggle.syncState();
+
+
         AppCenter.start(getApplication(), BuildConfig.APPCANTER_API_KEY,
                 Analytics.class, Crashes.class);
-
-
-
 
     }
 
@@ -30,9 +45,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
 
-        getSupportFragmentManager()
-                .beginTransaction()
-                .add(R.id.container, MinhaLocalizacaoFragment.newInstance())
-                .commit();
+
     }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.nav_percurso:
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .add(R.id.container, MinhaLocalizacaoFragment.newInstance())
+                        .commit();
+                break;
+        }
+
+        if(drawerLayout.isDrawerOpen(GravityCompat.START))
+            drawerLayout.closeDrawer(GravityCompat.START);
+
+        return true;
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START))
+            drawerLayout.closeDrawer(GravityCompat.START);
+        else
+            super.onBackPressed();
+    }
+
 }
